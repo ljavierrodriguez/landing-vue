05445fb2a9683d5ad82c502036930b77843145ee
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-08-04 20:30:40 -0400

    Landing Vue

---
+++ src/App.vue
@@ -0,0 +1,218 @@
+<template>
+  <div>
+    <!-- Navigation-->
+    <MainNavbar />
+    <!-- Header-->
+    <HeaderComponent :name="name" />
+    <!-- Content section 1-->
+    <section id="scroll">
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-lg-6 order-lg-2">
+            <div class="p-5">
+              <img
+                class="img-fluid rounded-circle"
+                src="assets/img/01.jpeg"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col-lg-6 order-lg-1">
+            <div class="p-5">
+              <h2 class="display-4">For those about to rock...</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                aliquid, mollitia odio veniam sit iste esse assumenda amet
+                aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                voluptatum molestiae adipisci, beatae obcaecati.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Content section 2-->
+    <section>
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-lg-6">
+            <div class="p-5">
+              <img
+                class="img-fluid rounded-circle"
+                src="assets/img/02.jpeg"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="p-5">
+              <h2 class="display-4">We salute you!</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                aliquid, mollitia odio veniam sit iste esse assumenda amet
+                aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                voluptatum molestiae adipisci, beatae obcaecati.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Content section 3-->
+    <section>
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-lg-6 order-lg-2">
+            <div class="p-5">
+              <img
+                class="img-fluid rounded-circle"
+                src="assets/img/03.jpeg"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div class="col-lg-6 order-lg-1">
+            <div class="p-5">
+              <h2 class="display-4">Let there be rock!</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                aliquid, mollitia odio veniam sit iste esse assumenda amet
+                aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                voluptatum molestiae adipisci, beatae obcaecati.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-black">
+      <div class="container px-5">
+        <p class="m-0 text-center text-white small">
+          Copyright &copy; Your Website 2022
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+import MainNavbar from './components/MainNavbar.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+
+export default {
+  name: "App",
+  components: {
+    'MainNavbar': MainNavbar,
+    'HeaderComponent': HeaderComponent
+  },
+  data(){
+    return {
+      name: 'Luis Rodriguez'
+    }
+  },
+  methods: {
+    handleChange: function(){
+
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  scroll-padding-top: 67px;
+}
+
+.btn-xl {
+  text-transform: uppercase;
+  padding: 1.5rem 3rem;
+  font-size: 0.9rem;
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+}
+
+.navbar-custom {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+.navbar-custom .navbar-brand {
+  text-transform: uppercase;
+  font-size: 1rem;
+  letter-spacing: 0.1rem;
+  font-weight: 700;
+}
+.navbar-custom .navbar-nav .nav-item .nav-link {
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+}
+
+header.masthead {
+  position: relative;
+  overflow: hidden;
+  padding-top: calc(7rem + 72px);
+  padding-bottom: 7rem;
+  background: linear-gradient(0deg, #ff6a00 0%, #ee0979 100%);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: scroll;
+  background-size: cover;
+}
+header.masthead .masthead-content {
+  z-index: 1;
+  position: relative;
+}
+header.masthead .masthead-content .masthead-heading {
+  font-size: 4rem;
+}
+header.masthead .masthead-content .masthead-subheading {
+  font-size: 2rem;
+}
+header.masthead .bg-circle {
+  z-index: 0;
+  position: absolute;
+  border-radius: 100%;
+  background: linear-gradient(0deg, #ee0979 0%, #ff6a00 100%);
+}
+header.masthead .bg-circle-1 {
+  height: 90rem;
+  width: 90rem;
+  bottom: -55rem;
+  left: -55rem;
+}
+header.masthead .bg-circle-2 {
+  height: 50rem;
+  width: 50rem;
+  top: -25rem;
+  right: -25rem;
+}
+header.masthead .bg-circle-3 {
+  height: 20rem;
+  width: 20rem;
+  bottom: -10rem;
+  right: 5%;
+}
+header.masthead .bg-circle-4 {
+  height: 30rem;
+  width: 30rem;
+  top: -5rem;
+  right: 35%;
+}
+
+@media (min-width: 992px) {
+  header.masthead {
+    padding-top: calc(10rem + 55px);
+    padding-bottom: 10rem;
+  }
+  header.masthead .masthead-content .masthead-heading {
+    font-size: 6rem;
+  }
+  header.masthead .masthead-content .masthead-subheading {
+    font-size: 4rem;
+  }
+}
+</style>
